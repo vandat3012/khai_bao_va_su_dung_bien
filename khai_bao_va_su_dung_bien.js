770b838f0b9e56f8d442ab7284eab5aa57d5484a
vandat3012 <luongvandat301220@gmail.com>
2023-11-09 16:59:23 +0700

    update bai tap

---
+++ khai_bao_va_su_dung_bien.js
@@ -0,0 +1,29 @@
+let a= 10;
+let b = 20;
+let c= 30;
+let d="ĐN";
+
+document.write('a =' +a);
+document.write('<br/>');
+document.write('b =' +b);
+document.write('<br/>');
+document.write('c =' +c);
+document.write('<br/>');
+document.write('d =' +d);
+// bài 2
+document.write('<br/>');
+let e=10;
+let f=20;
+let area=e*f;
+document.write('area =' +area);
+// bài 3
+document.write('<br/>');
+let g =prompt("Enter g");
+let h = prompt("Enter h");
+let result=g%h;
+  if (result == 0){
+      alert("g is the multiple of h");
+  }else{
+      alert("g is not multiple of h");
+  }
+
